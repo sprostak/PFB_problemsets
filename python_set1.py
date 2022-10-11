8e2d76f829ac6fb970bcefae70ff3329894551bf
student <student@info-14.cshl.edu>
2022-10-11 10:39:37 -0400

    added system arguments for variables of fave color etc

--- python_set1.py
+++ python_set1.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
-my_name = "Sarah Prostak"
+
+import sys
+
+my_name = sys.argv[1]
 print(my_name)
+
+fav_color = sys.argv[2]
+print(my_name+ "'s" + " favorite color is " + fav_color)
+
+fav_activity = sys.argv[3]
+print(my_name+"'s" + " favorite activity is " + fav_activity)
+
+fav_animal = sys.argv[4]
+print(my_name+ "'s" + " favorite animal is " + fav_animal)
