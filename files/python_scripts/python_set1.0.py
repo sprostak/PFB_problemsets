9510bbedd3339b9155c1a024685d84865a5bad25
student <student@info-14.cshl.edu>
2022-10-11 16:02:53 -0400

    updating file version numbers to order properly

---
+++ files/python_scripts/python_set1.0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+my_name = sys.argv[1]
+print(my_name)
+
+fav_color = sys.argv[2]
+print(my_name+ "'s" + " favorite color is " + fav_color)
+
+fav_activity = sys.argv[3]
+print(my_name+"'s" + " favorite activity is " + fav_activity)
+
+fav_animal = sys.argv[4]
+print(my_name+ "'s" + " favorite animal is " + fav_animal)
