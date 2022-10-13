dbf4d4cccd7d85eb3f8b17da07f502b57dc91d59
student <student@info-14.cshl.edu>
2022-10-12 20:46:08 -0400

    adding 5th file from python mod 4 problem set 1

--- files/python_scripts/python_set4.4.py
+++ files/python_scripts/python_set4.4.py
@@ -9,6 +9,13 @@ for i in list1:
 	print(i)
 	print(len(i), i, sep='\t') 
 	
-list2 = [
+list2 = [(len(i),i) for i in list1]
+print(list2) 
+
+list3 = [(list1.index(i), len(i), i) for i in list1]
+print(list3)
+
+
+
 
 
